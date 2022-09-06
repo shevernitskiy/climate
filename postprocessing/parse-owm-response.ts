b49a1289e275bbc5e6d0570fb3bb0aab8bd4d969
sheverniskiy <hwork@list.ru>
2022-09-06 08:53:06 +0300

    initial

---
+++ postprocessing/parse-owm-response.ts
@@ -0,0 +1,11 @@
+import { readJSON, writeJSON } from 'https://deno.land/x/flat@0.0.x/mod.ts'
+
+// The filename is the first invocation argument
+const filename = Deno.args[0] // Same name as downloaded_filename
+const data = await readJSON(filename)
+
+// Pluck a specific key off
+// and write it out to a different file
+// Careful! any uncaught errors and the workflow will fail, committing nothing.
+const newfile = `post${filename}`
+await writeJSON(newfile, { [Date.now()]: data })
